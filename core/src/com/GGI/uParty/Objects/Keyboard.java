bb781a30c5770bcb225f24a69f3123a9212ce143
Emmett <goodgameindustries@gmail.com>
2016-02-12 13:24:32 -0600

    large keyboard improvements

--- core/src/com/GGI/uParty/Objects/Keyboard.java
+++ core/src/com/GGI/uParty/Objects/Keyboard.java
@@ -57,136 +57,136 @@ public class Keyboard {
 	/**creates all the needed keys and sets their bounds*/
 	public void initKeys(){
 			keys.add(new Key("1","1",this));
-			keys.get(keys.size()-1).setBounds(w/60, 17*h/60, w/15, 2*h/60);
+			keys.get(keys.size()-1).setBounds(0, .825f*bounds.height, w/10, .15f*bounds.height);
 		
 			keys.add(new Key("2","2",this));
-			keys.get(keys.size()-1).setBounds(7*w/60, 17*h/60, w/15, 2*h/60);
+			keys.get(keys.size()-1).setBounds(.1f*w, .825f*bounds.height, w/10, .15f*bounds.height);
 			
 			keys.add(new Key("3","3",this));
-			keys.get(keys.size()-1).setBounds(13*w/60, 17*h/60, w/15, 2*h/60);
+			keys.get(keys.size()-1).setBounds(.2f*w, .825f*bounds.height, w/10, .15f*bounds.height);
 			
 			keys.add(new Key("4","4",this));
-			keys.get(keys.size()-1).setBounds(19*w/60, 17*h/60, w/15, 2*h/60);
+			keys.get(keys.size()-1).setBounds(.3f*w, .825f*bounds.height, w/10, .15f*bounds.height);
 			
 			keys.add(new Key("5","5",this));
-			keys.get(keys.size()-1).setBounds(25*w/60, 17*h/60, w/15, 2*h/60);
+			keys.get(keys.size()-1).setBounds(.4f*w, .825f*bounds.height, w/10, .15f*bounds.height);
 			
 			keys.add(new Key("6","6",this));
-			keys.get(keys.size()-1).setBounds(31*w/60, 17*h/60, w/15, 2*h/60);
+			keys.get(keys.size()-1).setBounds(.5f*w, .825f*bounds.height, w/10, .15f*bounds.height);
 			
 			keys.add(new Key("7","7",this));
-			keys.get(keys.size()-1).setBounds(37*w/60, 17*h/60, w/15, 2*h/60);
+			keys.get(keys.size()-1).setBounds(.6f*w, .825f*bounds.height, w/10, .15f*bounds.height);
 			
 			keys.add(new Key("8","8",this));
-			keys.get(keys.size()-1).setBounds(43*w/60, 17*h/60, w/15, 2*h/60);
+			keys.get(keys.size()-1).setBounds(.7f*w, .825f*bounds.height, w/10, .15f*bounds.height);
 			
 			keys.add(new Key("9","9",this));
-			keys.get(keys.size()-1).setBounds(49*w/60, 17*h/60, w/15, 2*h/60);
+			keys.get(keys.size()-1).setBounds(.8f*w, .825f*bounds.height, w/10, .15f*bounds.height);
 			
 			keys.add(new Key("0","0",this));
-			keys.get(keys.size()-1).setBounds(55*w/60, 17*h/60, w/15, 2*h/60);
+			keys.get(keys.size()-1).setBounds(.9f*w, .825f*bounds.height, w/10, .15f*bounds.height);
 			
 			keys.add(new Key("q","Q",this));
-			keys.get(keys.size()-1).setBounds(1*w/60, 13*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(0, .6f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("w","W",this));
-			keys.get(keys.size()-1).setBounds(7*w/60, 13*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.1f*w, .6f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("e","E",this));
-			keys.get(keys.size()-1).setBounds(13*w/60, 13*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.2f*w, .6f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("r","R",this));
-			keys.get(keys.size()-1).setBounds(19*w/60, 13*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.3f*w, .6f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("t","T",this));
-			keys.get(keys.size()-1).setBounds(25*w/60, 13*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.4f*w, .6f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("y","Y",this));
-			keys.get(keys.size()-1).setBounds(31*w/60, 13*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.5f*w, .6f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("u","U",this));
-			keys.get(keys.size()-1).setBounds(37*w/60, 13*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.6f*w, .6f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("i","I",this));
-			keys.get(keys.size()-1).setBounds(43*w/60, 13*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.7f*w, .6f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("o","O",this));
-			keys.get(keys.size()-1).setBounds(49*w/60, 13*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.8f*w, .6f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("p","P",this));
-			keys.get(keys.size()-1).setBounds(55*w/60, 13*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.9f*w, .6f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("a","A",this));
-			keys.get(keys.size()-1).setBounds(4*w/60, 9*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.05f*w, .4f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("s","S",this));
-			keys.get(keys.size()-1).setBounds(10*w/60, 9*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.15f*w, .4f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("d","D",this));
-			keys.get(keys.size()-1).setBounds(16*w/60, 9*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.25f*w, .4f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("f","F",this));
-			keys.get(keys.size()-1).setBounds(22*w/60, 9*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.35f*w, .4f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("g","G",this));
-			keys.get(keys.size()-1).setBounds(28*w/60, 9*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.45f*w, .4f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("h","H",this));
-			keys.get(keys.size()-1).setBounds(34*w/60, 9*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.55f*w, .4f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("j","J",this));
-			keys.get(keys.size()-1).setBounds(40*w/60, 9*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.65f*w, .4f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("k","K",this));
-			keys.get(keys.size()-1).setBounds(46*w/60, 9*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.75f*w, .4f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("l","L",this));
-			keys.get(keys.size()-1).setBounds(52*w/60, 9*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.85f*w, .4f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("Shift","Shift",this));
-			keys.get(keys.size()-1).setBounds(1*w/60, 5*h/60, 3*w/30, 3*h/60);
+			keys.get(keys.size()-1).setBounds(0, .2f*bounds.height, .15f*w, .2f*bounds.height);
 			
 			keys.add(new Key("z","Z",this));
-			keys.get(keys.size()-1).setBounds(10*w/60, 5*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.15f*w, .2f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("x","X",this));
-			keys.get(keys.size()-1).setBounds(16*w/60, 5*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.25f*w, .2f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("c","C",this));
-			keys.get(keys.size()-1).setBounds(22*w/60, 5*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.35f*w, .2f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("v","V",this));
-			keys.get(keys.size()-1).setBounds(28*w/60, 5*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.45f*w, .2f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("b","B",this));
-			keys.get(keys.size()-1).setBounds(34*w/60, 5*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.55f*w, .2f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("n","N",this));
-			keys.get(keys.size()-1).setBounds(40*w/60, 5*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.65f*w, .2f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("m","M",this));
-			keys.get(keys.size()-1).setBounds(46*w/60, 5*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.75f*w, .2f*bounds.height, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("Back","Back",this));
-			keys.get(keys.size()-1).setBounds(53*w/60, 5*h/60, 3*w/30, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.85f*w, .2f*bounds.height, .15f*w, .2f*bounds.height);
 			
 			keys.add(new Key("@","@",this));
-			keys.get(keys.size()-1).setBounds(1*w/60, 1*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(0, 0, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("-","-",this));
-			keys.get(keys.size()-1).setBounds(7*w/60, 1*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.1f*w, 0, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("_","_",this));
-			keys.get(keys.size()-1).setBounds(13*w/60, 1*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.2f*w, 0, w/10, .2f*bounds.height);
 			
 			keys.add(new Key(".",".",this));
-			keys.get(keys.size()-1).setBounds(43*w/60, 1*h/60, w/15, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.7f*w, 0, w/10, .2f*bounds.height);
 			
 			keys.add(new Key("Enter","Enter",this));
-			keys.get(keys.size()-1).setBounds(49*w/60, 1*h/60, 5*w/30, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.8f*w, 0, .2f*w, .2f*bounds.height);
 			
 			keys.add(new Key("Space","Space",this));
-			keys.get(keys.size()-1).setBounds(19*w/60, 1*h/60, 11*w/30, 3*h/60);
+			keys.get(keys.size()-1).setBounds(.3f*w, 0, .4f*w, .2f*bounds.height);
 			
 	}
 	
@@ -224,6 +224,9 @@ public class Keyboard {
 				else{
 					System.out.println("A key was typed");
 					inp.keyTyped(keys.get(i).getText().charAt(0));
+					if(!keys.get(i).getText().equals("Shift")&&isShifted){
+						shift();
+					}
 				}
 				break;
 			}
@@ -259,7 +262,7 @@ public class Keyboard {
 		
 		
 		if(theta==Math.PI/2){
-			//pic.draw(u.assets.logo1024, 9*w/20, (float) (1*h/60), w/10,w/10);
+			//pic.draw(u.assets.logo1024, 9*w/20, (float) (0), w/10,w/10);
 			for(int i = 0; i < keys.size(); i++){
 				keys.get(i).draw(pic, 1);
 			}
